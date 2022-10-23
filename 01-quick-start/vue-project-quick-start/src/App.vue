7f55ace434702b817ac76e5d239995f589a4661b
arisemarcher <2543061999@qq.com>
2022-10-23 16:53:25 +0800

    列表渲染

--- 01-quick-start/vue-project-quick-start/src/App.vue
+++ 01-quick-start/vue-project-quick-start/src/App.vue
@@ -4,6 +4,7 @@ import ReactiveFundamentals from './components/02-reactivity-fundamentals.vue';
 import ComputedProperties from './components/03-computed-properties.vue'
 import ClassAndStyleBinding from './components/04-class-and-style-binding.vue'
 import ConditionalRendering from './components/05-conditional-rendering.vue'
+import ListRendering from './components/06-list-rendering.vue'
 </script>
 
 <template>
@@ -11,7 +12,8 @@ import ConditionalRendering from './components/05-conditional-rendering.vue'
   <!-- <ReactiveFundamentals /> -->
   <!-- <ComputedProperties /> -->
   <!-- <ClassAndStyleBinding class="auto-class" /> -->
-  <ConditionalRendering />
+  <!-- <ConditionalRendering /> -->
+  <ListRendering />
 </template>
 
 <style scoped>
---
+++ 01-quick-start/vue-project-quick-start/src/components/06-list-rendering.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    当 v-if 和 v-for 同时存在于一个元素上的时候，v-if 会首先被执行
+  </div>
+  <li v-for="(item, index) in items">
+    {{ parentMessage }} - {{ index }} - {{ item.message }}
+  </li>
+
+  <ul>
+    <li v-for="(value, key, index) in myObject">
+      {{ index }}. {{ key }}: {{ value }}
+    </li>
+  </ul>
+
+  <ul>
+    <template v-for="item in items">
+      <li>{{ item.message }}</li>
+      <li class="divider" role="presentation"></li>
+    </template>
+  </ul>
+
+  <ul v-for="numbers in sets">
+    <li v-for="n in even(numbers)">{{ n }}</li>
+  </ul>
+
+
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+const parentMessage = ref('Parent');
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }]);
+
+const myObject = reactive({
+  title: 'How to do lists in Vue',
+  author: 'Jane Doe',
+  publishedAt: '2016-04-10'
+});
+
+const sets = ref([
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10]
+])
+
+function even(numbers) {
+  return numbers.filter((number) => number % 2 === 0)
+};
+
+</script>
+
+<style scoped>
+  
+</style>
