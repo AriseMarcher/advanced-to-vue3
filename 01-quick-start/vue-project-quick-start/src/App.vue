88315f261ddbc091fd50eb231c6176758f1e5fe2
arisemarcher <2543061999@qq.com>
2022-10-23 16:45:26 +0800

    条件渲染

--- 01-quick-start/vue-project-quick-start/src/App.vue
+++ 01-quick-start/vue-project-quick-start/src/App.vue
@@ -3,13 +3,15 @@ import TemplateSyntax from './components/01-template-syntax.vue';
 import ReactiveFundamentals from './components/02-reactivity-fundamentals.vue';
 import ComputedProperties from './components/03-computed-properties.vue'
 import ClassAndStyleBinding from './components/04-class-and-style-binding.vue'
+import ConditionalRendering from './components/05-conditional-rendering.vue'
 </script>
 
 <template>
   <!-- <TemplateSyntax /> -->
   <!-- <ReactiveFundamentals /> -->
   <!-- <ComputedProperties /> -->
-  <ClassAndStyleBinding class="auto-class" />
+  <!-- <ClassAndStyleBinding class="auto-class" /> -->
+  <ConditionalRendering />
 </template>
 
 <style scoped>
---
+++ 01-quick-start/vue-project-quick-start/src/components/05-conditional-rendering.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="awesome = !awesome">Toggle</button>
+
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+
+    <template v-if="ok">
+      <h1>Title</h1>
+      <p>Paragraph 1</p>
+      <p>Paragraph 2</p>
+    </template>
+
+    <div v-if="type === 'A'">
+      A
+    </div>
+    <div v-else-if="type === 'B'">
+      B
+    </div>
+    <div v-else-if="type === 'C'">
+      C
+    </div>
+    <div v-else>
+      Not A/B/C
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const awesome = ref(true);
+const ok = ref(true);
+
+const type = ref('');
+</script>
+
+<style scoped>
+  
+</style>
