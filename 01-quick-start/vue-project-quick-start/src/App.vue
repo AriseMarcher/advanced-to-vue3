5d39c3414f5b7ff392c10b2637fceaec4ad13dd0
arisemarcher <2543061999@qq.com>
2022-10-23 17:02:38 +0800

    事件处理

--- 01-quick-start/vue-project-quick-start/src/App.vue
+++ 01-quick-start/vue-project-quick-start/src/App.vue
@@ -5,6 +5,7 @@ import ComputedProperties from './components/03-computed-properties.vue'
 import ClassAndStyleBinding from './components/04-class-and-style-binding.vue'
 import ConditionalRendering from './components/05-conditional-rendering.vue'
 import ListRendering from './components/06-list-rendering.vue'
+import EventHandling from './components/07-event-handling.vue'
 </script>
 
 <template>
@@ -13,7 +14,8 @@ import ListRendering from './components/06-list-rendering.vue'
   <!-- <ComputedProperties /> -->
   <!-- <ClassAndStyleBinding class="auto-class" /> -->
   <!-- <ConditionalRendering /> -->
-  <ListRendering />
+  <!-- <ListRendering /> -->
+  <EventHandling />
 </template>
 
 <style scoped>
---
+++ 01-quick-start/vue-project-quick-start/src/components/07-event-handling.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- 使用特殊的 $event 变量 -->
+  <button @click="warn('Form cannot be submitted yet.', $event)">
+    Submit
+  </button>
+
+  <!-- 使用内联箭头函数 -->
+  <button @click="(event) => warn('Form cannot be submitted yet.', event)">
+    Submit
+  </button>
+</template>
+
+<script setup>
+function warn(message, event) {
+  // 这里可以访问原生事件
+  if (event) {
+    console.log(event)
+    event.preventDefault()
+  }
+  alert(message)
+};
+// 事件修饰符
+// @click.prevent.self 会阻止元素及其子元素的所有点击事件的默认行为而
+// @click.self.prevent 则只会阻止对元素本身的点击事件的默认行为。
+</script>
+
+<style scoped>
+  
+</style>
